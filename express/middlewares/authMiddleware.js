6c08e3af081f7ee1d907678fc74a5dcab7e69937
daedara007 <daedara007@gmail.com>
2026-09-04 04:05:27 +0800

    update

--- express/middlewares/authMiddleware.js
+++ express/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'rahasia_jwt_super_aman_123';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const validasiToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
